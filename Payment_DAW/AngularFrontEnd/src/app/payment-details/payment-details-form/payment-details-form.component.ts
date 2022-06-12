12a64abedeee2bb86a5c0fb72a67570f2a66a137
raresps <rarespetrustn@gmail.com>
2022-06-12 03:57:35 +0300

    payment details form

---
+++ Payment_DAW/AngularFrontEnd/src/app/payment-details/payment-details-form/payment-details-form.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { PaymentDetail } from 'src/app/shared/payment-detail.model';
+import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
+
+@Component({
+  selector: 'app-payment-details-form',
+  templateUrl: './payment-details-form.component.html',
+  styles: [
+  ]
+})
+export class PaymentDetailsFormComponent implements OnInit {
+
+  constructor(public service:PaymentDetailService,private toastr:ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form:NgForm){
+
+    if(this.service.formData.id == 0)
+      this.insertRecord(form)
+    else
+      this.updateRecord(form)
+  }
+
+  insertRecord(form:NgForm){
+
+    this.service.postPaymentDetails().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList()
+        this.toastr.success('Submited succesfully','Payment details register')
+      },
+      err => { console.log(err); }
+    );
+  }
+
+  updateRecord(form: NgForm){
+
+    this.service.putPaymentDetails().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList()
+        this.toastr.info('Updated succesfully','Payment details register')
+      },
+      err => { console.log(err); }
+    );
+  }
+
+  resetForm(form:NgForm){
+
+    form.form.reset()
+    this.service.formData = new PaymentDetail();
+  }
+}
