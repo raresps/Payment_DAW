cce3b0821709064e89990d8ec48966e59be96e4d
raresps <rarespetrustn@gmail.com>
2022-06-12 03:35:43 +0300

    Add Database migrations, and controller

---
+++ Payment_DAW/Controllers/PaymentDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Payment_DAW.Controllers
+{
+    public class PaymentDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
